41610f692d77ccb28c34525c15c9bfc37a1d3b76
LAPTOP-LV05Q557\Peter <peter.yudhistiraa@gmail.com>
2025-05-02 01:03:14 +0700

    Changes in table checklists and typos in insert categories

--- queries/insert/insert_categories.sql
+++ queries/insert/insert_categories.sql
@@ -1,4 +1,5 @@
-INSERT INTO checklist.categories(id, category_code, category_name)VALUES (1,'PRE,'Preparation);
-INSERT INTO checklist.categories(id, category_code, category_name)VALUES (2,'IPL,'Implementation);
-INSERT INTO checklist.categories(id, category_code, category_name)VALUES (3,'VRF,'Verification);
-INSERT INTO checklist.categories(id, category_code, category_name)VALUES (4,'HKI,'Housekeeping);
+INSERT INTO checklist.categories(id, category_code, category_name)VALUES (1,'PRE','Preparation');
+INSERT INTO checklist.categories(id, category_code, category_name)VALUES (2,'IPL','Implementation');
+INSERT INTO checklist.categories(id, category_code, category_name)VALUES (3,'VRF','Verification');
+INSERT INTO checklist.categories(id, category_code, category_name)VALUES (4,'HKI','Housekeeping');
+INSERT INTO checklist.categories(id, category_code, category_name)VALUES (5,'FAL','Fallback');
